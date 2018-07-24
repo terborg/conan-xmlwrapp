c281af2fd108612de190447de6dee2066f353f83
Rutger ter Borg <rutger.terborg@artibit.com>
2018-07-24 10:50:24 +0200

    minor cleanups

--- conanfile.py
+++ conanfile.py
@@ -22,25 +22,14 @@ class XMLWrappConan(ConanFile):
     def source(self):
         tools.get( "https://github.com/vslavik/xmlwrapp/releases/download/v0.9.0/xmlwrapp-0.9.0.tar.gz", 
                    sha1="6fa3193a013b7d29bb22e220e9bd015bc14eb11e" )
-        
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        #tools.replace_in_file("rapidcheck/CMakeLists.txt", "project(rapidcheck CXX)",
-                              #'''project(rapidcheck CXX)
-#include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-#conan_basic_setup()''')
 
     def source_path( self ):
         return os.path.join( self.source_folder, self.name + '-' + self.version )
 
     def build(self):
-        #source_subfolder = name + '-' + version
-        
-        print tools.env
         
         env_build = AutoToolsBuildEnvironment(self)
-        env_build.fpic = self.options.fPIC   # should go automatically
+        env_build.fpic = self.options.fPIC
         
         configure_args = [ '--disable-docs', '--disable-tests' ]
         if not self.options.shared:
@@ -52,7 +41,6 @@ class XMLWrappConan(ConanFile):
     def package(self):
         
         self.copy( "*.h", dst="include", src= os.path.join( self.source_path(), "include" )  )
-        #self.copy("*.hpp", dst="include", src="include")
         
         self.copy( "*xmlwrapp.lib", dst="lib", keep_path=False )
         self.copy( "*.dll", dst="bin", keep_path=False )
